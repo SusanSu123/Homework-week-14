754b570569dea77bc13e6b5a89670d7d18559ddf
Susan Su <suchangau@hotmail.com>
2021-06-07 12:22:50 +1000

    signup

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,17 +2,16 @@ async function signupFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
+    //const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-    const twitter = document.querySelector('#twitter-signup').value.trim();
-    const github = document.querySelector('#github-signup').value.trim();
+
   
-    if (username && email && password) {
+    if (username &&  password) {
       const response = await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
           username,
-          email,
+          //email,
           password
         }),
         headers: { 'Content-Type': 'application/json' }
